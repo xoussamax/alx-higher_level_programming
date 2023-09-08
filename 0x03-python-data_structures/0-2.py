8a8b1ac47f508bfd8997614f07ce99141de98eb2
Oussama <oussama@hotmail.com>
2023-09-08 10:37:39 -0700

    t

--- 0x03-python-data_structures/0-2.py
+++ 0x03-python-data_structures/0-2.py
@@ -4,5 +4,7 @@ def print_list_integer1(my_list=[]):
     print("{}".format(my_list[2]))
     print("{}".format(my_list[3]))
     print("{}".format(my_list[4]))
+
+
 def print_list_integer2(my_list=[]):
     print("{}\n{}\n{}\n{}\n{}".format(my_list[0], my_list[1], my_list[2], my_list[3], my_list[4]))
