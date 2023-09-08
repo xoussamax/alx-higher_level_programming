c4cd810290118cdfc678f154bae185522d627906
Oussama <oussama@hotmail.com>
2023-09-08 10:15:47 -0700

    t

---
+++ 0x03-python-data_structures/0-2.py
@@ -0,0 +1,6 @@
+def print_list_integer1(my_list=[]):
+    print("{}".format(my_list[0]))
+    print("{}".format(my_list[1]))
+    print("{}".format(my_list[2]))
+    print("{}".format(my_list[3]))
+    print("{}".format(my_list[4]))
--- 0x03-python-data_structures/0-main.py
+++ 0x03-python-data_structures/0-main.py
@@ -1,4 +1,5 @@
 print_list_integer = __import__('0-print_list_integer').print_list_integer
-
+print_list_integer1 = __import__('0-2').print_list_integer1
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
+print_list_integer1(my_list)
