0db5decfcc95cbfb495beef3e4db6725839443db
Oussama <oussama@hotmail.com>
2023-09-10 11:03:41 -0700

    t

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,3 @@
 def print_list_integer(my_list=[]):
-    list = my_list[0]
-    list = my_list[1]
-    list = my_list[2]
-    list = my_list[2]
-    list = my_list[3]
-
-    for list in my_list:
-     print(list)
+    for item in my_list:
+        print(item)
