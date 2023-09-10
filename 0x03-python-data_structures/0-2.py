414a7ecef0710ea3760eb0c7d1c0693cce6456e6
Oussama <oussama@hotmail.com>
2023-09-10 10:41:43 -0700

    t

--- 0x03-python-data_structures/0-2.py
+++ 0x03-python-data_structures/0-2.py
@@ -4,7 +4,10 @@ def print_list_integer1(my_list=[]):
     print("{}".format(my_list[2]))
     print("{}".format(my_list[3]))
     print("{}".format(my_list[4]))
-
-
-def print_list_integer2(my_list=[]):
+# line 8 too long
     print("{}\n{}\n{}\n{}\n{}".format(my_list[0], my_list[1], my_list[2], my_list[3], my_list[4]))
+# no new line
+    print(my_list[0], my_list[1], my_list[2], my_list[3], my_list[4])
+
+    for item in my_list:
+        print(item)
--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,2 +1,7 @@
 def print_list_integer(my_list=[]):
-    print(f'{1}\n{2}\n{3}\n{4}\n{5}')
+
+    print("{}".format(my_list[0]))
+    print("{}".format(my_list[1]))
+    print("{}".format(my_list[2]))
+    print("{}".format(my_list[3]))
+    print("{}".format(my_list[4]))
